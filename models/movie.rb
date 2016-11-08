4c0ba91c751316dca358db7a285277723ab92758
vagrant <vagrant@lighthouse-labs>
2016-11-08 20:56:11 +0000

    updated example

--- models/movie.rb
+++ models/movie.rb
@@ -1,18 +1,19 @@
+
 class Movie < ActiveRecord::Base
   has_many :ratings
   has_many :reviewers, through: :ratings
 
-  validates :year,
-    numericality: { less_than_or_equal_to: Date.today.year }
-
-  validates :title,
-    length: { maximum: 25, minimum: 3 } # you can customize error messages
+  # validates :year,
+  #   numericality: { less_than_or_equal_to: Date.today.year }
+  #
+  # validates :title,
+  #   length: { maximum: 25, minimum: 3 } # you can customize error messages
 
   # validate :check_some_stuff # custom validations
-  after_create :movie_just_created
+  # after_create :movie_just_created
 
-  def movie_just_created
-    puts "a movie, with title #{title}, has just been created."
-  end
+  # def movie_just_created
+  #   puts "a movie, with title #{title}, has just been created."
+  # end
 
 end
--- setup.rb
+++ setup.rb
@@ -13,45 +13,47 @@ ActiveRecord::Base.establish_connection(
   encoding: 'unicode',
   pool: 5,
   database: 'active_record_ratings',
-  username: 'faisal',
-  password: '',
+  username: 'development',
+  password: 'development',
   host: 'localhost',
   port: 5432,
   min_messages: 'error'
 )
 puts "CONNECTED"
 
-puts "Setting up Database (recreating tables) ..."
+# puts "Setting up Database (recreating tables) ..."
 
-# ActiveRecord::Schema.define do
-#   # check if these tables exist, completely remove them if they do
-#   drop_table :movies, force: :cascade if ActiveRecord::Base.connection.table_exists?(:movies)
-#   drop_table :reviewers, force: :cascade if ActiveRecord::Base.connection.table_exists?(:reviewers)
-#   drop_table :ratings, force: :cascade if ActiveRecord::Base.connection.table_exists?(:ratings)
-#
-#   # migrations
-#   create_table :movies do |m|
-#     m.column :title, :string
-#     m.column :year, :integer
-#     m.column :director, :string
-#     m.timestamps
-#   end
-#
-#   create_table :reviewers do |r|
-#     r.column :name, :string
-#     r.timestamps
-#   end
-#
-#   create_table :ratings do |r|
-#     # r.references :movie
-#     # r.references :reviewer
-#     r.column :movie_id, :integer
-#     r.column :reviewer_id, :integer
-#     r.column :stars, :integer
-#     r.column :rating_date, :date
-#     r.timestamps
-#   end
-#
-# end
+ActiveRecord::Schema.define do
+  # check if these tables exist, completely remove them if they do
+  drop_table :movies, force: :cascade if ActiveRecord::Base.connection.table_exists?(:movies)
+  drop_table :reviewers, force: :cascade if ActiveRecord::Base.connection.table_exists?(:reviewers)
+  drop_table :ratings, force: :cascade if ActiveRecord::Base.connection.table_exists?(:ratings)
+
+  # migrations
+  create_table :movies do |m|
+    m.column :title, :string
+    m.column :year, :integer
+    m.column :director, :string
+    m.timestamps
+  end
+
+  create_table :reviewers do |r|
+    r.column :name, :string
+    r.timestamps
+  end
+
+  create_table :ratings do |r|
+    r.references :movie
+    r.references :reviewer
+    # r.column :movie_id, :integer
+    # r.column :reviewer_id, :integer
+    r.column :stars, :integer
+    # r.column :rating_date, :date
+    r.timestamps
+  end
+
+end
 
 puts "Setup DONE"
+
+binding.pry
